7bbfaae49f421b4c95b83b1808bd7a9b2f577f37
Ponanarutt <123799385+Ponanarutt@users.noreply.github.com>
2024-12-18 17:28:35 +0700

    Final

--- beginfull/lib/config/db.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-
-export const ConnectDB = async () => {
-    await mongoose.connect('mongodb+srv://Admin:ftwZFNWRXZ1fSUEk@cluster0.u3njo.mongodb.net/');
-    console.log("DB Connected")
-}
